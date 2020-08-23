4422ea69147d849868a258a25a626e1ee6383711
random guy <paulpaul115@rocketmail.com>
2020-08-23 20:41:48 +0900

    Add files via upload

---
+++ microbit_chat/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.received_text = New System.Windows.Forms.TextBox()
+        Me.str_out = New System.Windows.Forms.TextBox()
+        Me.Send = New System.Windows.Forms.Button()
+        Me.port_select = New System.Windows.Forms.ComboBox()
+        Me.port = New System.Windows.Forms.Button()
+        Me.serial = New System.IO.Ports.SerialPort(Me.components)
+        Me.clrtxt = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'received_text
+        '
+        resources.ApplyResources(Me.received_text, "received_text")
+        Me.received_text.Name = "received_text"
+        Me.received_text.ReadOnly = True
+        '
+        'str_out
+        '
+        resources.ApplyResources(Me.str_out, "str_out")
+        Me.str_out.Name = "str_out"
+        '
+        'Send
+        '
+        resources.ApplyResources(Me.Send, "Send")
+        Me.Send.Name = "Send"
+        Me.Send.UseVisualStyleBackColor = True
+        '
+        'port_select
+        '
+        Me.port_select.FormattingEnabled = True
+        resources.ApplyResources(Me.port_select, "port_select")
+        Me.port_select.Name = "port_select"
+        '
+        'port
+        '
+        resources.ApplyResources(Me.port, "port")
+        Me.port.Name = "port"
+        Me.port.UseVisualStyleBackColor = True
+        '
+        'serial
+        '
+        Me.serial.PortName = "portID"
+        '
+        'clrtxt
+        '
+        resources.ApplyResources(Me.clrtxt, "clrtxt")
+        Me.clrtxt.Name = "clrtxt"
+        Me.clrtxt.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.clrtxt)
+        Me.Controls.Add(Me.port)
+        Me.Controls.Add(Me.port_select)
+        Me.Controls.Add(Me.Send)
+        Me.Controls.Add(Me.str_out)
+        Me.Controls.Add(Me.received_text)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents str_out As TextBox
+    Friend WithEvents Send As Button
+    Friend WithEvents port_select As ComboBox
+    Friend WithEvents port As Button
+    Friend WithEvents serial As IO.Ports.SerialPort
+    Friend WithEvents received_text As TextBox
+    Friend WithEvents clrtxt As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ microbit_chat/Form1.vb
@@ -0,0 +1,112 @@
+﻿Imports System.IO.Ports
+Imports System.Text
+Public Class Form1
+
+    Dim strIn = "", strOut = "", portID = "COM1"
+    Dim command As Integer
+
+
+
+
+
+    Private Sub port_Click(sender As Object, e As EventArgs) Handles port.Click
+        portID = port_select.SelectedItem
+        If portID IsNot Nothing Then
+            serial.PortName = portID
+            serial.BaudRate = 115200
+            serial.DataBits = 8
+            serial.Parity = Parity.None
+            serial.StopBits = StopBits.One
+            serial.Encoding = Encoding.UTF8
+            serial.Open()
+            received_text.AppendText("Port set : " + portID + Environment.NewLine)
+            port.Enabled = False
+            Send.Enabled = True
+            str_out.Enabled = True
+            clrtxt.Enabled = True
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Send.Click
+        If str_out.Text IsNot "" Then
+            serial_out(str_out.Text)
+            strOut = " >> " + str_out.Text
+            received_text.AppendText(strOut + Environment.NewLine)
+            strOut = ""
+            str_out.Clear()
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        GetSerialPortNames()
+        received_text.TextAlign = HorizontalAlignment.Left
+
+    End Sub
+
+    Sub serial_out(data As String)
+
+        serial.WriteLine(data)
+
+    End Sub
+    Private Delegate Sub Delegate_RcvDataToTextBox(data As String)
+
+    Private Sub str_out_Enter(sender As Object, e As EventArgs) Handles str_out.Enter
+        str_out.Clear()
+
+    End Sub
+
+
+
+    Private Sub port_select_TextChanged(sender As Object, e As EventArgs) Handles port_select.TextChanged
+        port.Enabled = True
+
+    End Sub
+
+    Sub serial_DataReceived(ByVal sender As System.Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles serial.DataReceived
+        If serial.IsOpen = False Then
+            Return
+        End If
+
+        Try
+
+            Dim data As String
+            data = serial.ReadLine()
+            Invoke(New Delegate_RcvDataToTextBox(AddressOf Me.RcvDataToTextBox), data)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles clrtxt.Click
+        received_text.Clear()
+
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        If serial.IsOpen And True Then
+            serial.Close()
+        End If
+    End Sub
+
+
+
+    Private Sub RcvDataToTextBox(data As String)
+
+        If IsNothing(data) = False Then
+            received_text.AppendText(data + Environment.NewLine)
+        End If
+
+    End Sub
+    Sub GetSerialPortNames()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            port_select.Items.Add(sp)
+        Next
+    End Sub
+End Class
